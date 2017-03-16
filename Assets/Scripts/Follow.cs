3fb24036ae23384e5a2c76c88b0399cccae72ac6
alexstat123 <alexander.statsenko198@gmail.com>
2017-03-16 21:28:26 +0000

    side force, rope destroy, new camera angle

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -8,17 +8,18 @@ public class Follow : MonoBehaviour {
 	public float offsetY;
 
 	//public float transitionSpeed;
-	
+
 	public GameObject target;
-	
-	
+
+
 	// Update is called once per frame
 	void FixedUpdate () {
 
 		var x = target.transform.position.x + offsetX;
 		var y = target.transform.position.y + offsetY;
-		transform.position = new Vector3(x, y, transform.position.z);
-	
+		var z = target.transform.position.z;
+		transform.position = new Vector3(x, y, z); // was here transform.position.z
+
 		//float step = transitionSpeed * Time.deltaTime;
 		//transform.position = Vector3.MoveTowards(transform.position, new Vector3(x, y, transform.position.z), step);
 	}
