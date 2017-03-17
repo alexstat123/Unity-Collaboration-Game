d30f7eba1338d6388d615e597e80c9b7d2a4dba2
alexstat123 <alexander.statsenko198@gmail.com>
2017-03-17 22:36:28 +0000

    zoom function

---
+++ Assets/ZoomScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomScript : MonoBehaviour {
+
+	public Follow followcamerscript;
+	public int initialZoom=25;
+	public int MinZoom = 25;
+	public int MaxZoom = 50;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		zoomfunction ();
+		Debug.Log (initialZoom);
+	}
+
+
+
+	void zoomfunction()
+	{
+		followcamerscript.offsetX = initialZoom;
+
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			if(initialZoom <MaxZoom)
+			{
+				initialZoom++;
+			}
+
+
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			if(initialZoom > MinZoom)
+			{
+				initialZoom--;
+			}
+
+		}
+
+	}
+}
