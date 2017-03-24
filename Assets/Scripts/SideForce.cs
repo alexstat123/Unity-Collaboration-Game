f59c0b1473875a06c1102132612669135c3f9287
alexstat123 <alexander.statsenko198@gmail.com>
2017-03-24 22:26:40 +0000

    cannon firing projectiles

--- Assets/Scripts/SideForce.cs
+++ Assets/Scripts/SideForce.cs
@@ -1,1 +1,32 @@
-﻿using System.Collections; using System.Collections.Generic; using UnityEngine;  public class SideForce : MonoBehaviour { 	public float thrust; 	private Rigidbody rb; 	public bool Sideforce=false; 	public bool forwardforce = false;  	void Start () { 		rb = GetComponent<Rigidbody>(); 	}  	void FixedUpdate() { 		//rb.AddForce(Vector3.forward * thrust * Time.deltaTime); 		//rb.AddForce (thrust * Time.deltaTime,0,0); 		//rb.AddForce(new Vector3(0,thrust * Time.deltaTime,0));  		//rb.AddForce(Vector3. * thrust * Time.deltaTime); 		if(forwardforce==true) 		{ 			rb.AddForce(-1,0, 0, ForceMode.Impulse); 		}  		if(Sideforce==true) 		{ 			rb.AddForce(0,0, 1, ForceMode.Impulse); 		} 	} } 
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideForce : MonoBehaviour {
+	public float thrust;
+	private Rigidbody rb;
+	public bool Sideforce=false;
+	public bool forwardforce = false;
+	public int impulse = -1;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate() {
+		//rb.AddForce(Vector3.forward * thrust * Time.deltaTime);
+		//rb.AddForce (thrust * Time.deltaTime,0,0);
+		//rb.AddForce(new Vector3(0,thrust * Time.deltaTime,0));
+
+		//rb.AddForce(Vector3. * thrust * Time.deltaTime);
+		if(forwardforce==true)
+		{
+			rb.AddForce(impulse,0, 0, ForceMode.Impulse);
+		}
+
+		if(Sideforce==true)
+		{
+			rb.AddForce(0,0, 1, ForceMode.Impulse);
+		}
+	}
+}
---
+++ Assets/Spawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+	public GameObject shell;
+	int counter=0;
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		counter++;
+
+		if(counter %10 ==0){Instantiate (shell);}
+	}
+}
